3a1deb00c483ceac9fd528ea31ee577023fd7b87
John Christopher <jgchristopher@gmail.com>
2021-07-31 12:05:43 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # alfred-bunch-workflow
-An Alfred workflow to Open/Toggle bunches and manage preferences for Bunch
+
+> Run bunches using [Brett Terpstra's awesome Bunch app](https://brettterpstra.com/projects/bunch/) in macOS
+
+## Dependencies
+- [Node.js](https://nodejs.org) 4+
+- Alfred [Powerpack](https://www.alfredapp.com/powerpack/)
+
+## Install
+```
+
+$ npm install --global alfred-bunch-workflow
+
+```
+
+## Configuration
+
+1. Open Alfred Preferences. 
+2. Click on the "Workflows" tab and find Bunch.
+3. Click on the "Configure workflow and variables" button.
+4. Add the path to your Bunches folder in Workflow Environment Variables:
+    - `bunchLocation`: `/Users/yourname/bunches`
+5. (Optional) Change bunch toggle to true if you want the Alfred Workflow to open/close the bunch based on the bunches state
+    - `bunchToggle`: `true`
+5. Click "Save"
+
+## Usage
+
+- In Alfred, type `bu`. 
+    - Press <kbd>Enter</kbd> to open the Bunch preference pane.
+    - Press <kbd>⌘2</kbd> to refresh your list of bunch files
+- Continue typing a bunch name to filter the returned list. you can further filter the list by typing a bunch name
+    - Press <kbd>Enter</kbd> to run the selected bunch.
+
+## Created Using
+
+- [Alfy](https://github.com/sindresorhus/alfy) – Create Alfred workflows with ease
+
+## Inspired By
+- [Jay Miller](https://kjaymiller.com)  
+- [Matthew Johnson](https://github.com/mttjhn/alfred-bunch)
+
+## License
+
+MIT © [John Christopher](https://github.com/jgchristopher/alfred-bunch-workflow)  
+
---
+++ index.js
@@ -0,0 +1,58 @@
+const alfy = require('alfy');
+const glob = require("glob");
+
+const bunchFolder = process.env.bunchesLocation;
+const bunchToggle = (process.env.bunchToggle === 'true') ? true : false;
+
+const bunchCommand = bunchToggle === true ? 'toggle' : 'open';
+
+const bunchSettings = [
+    {
+        title: "Bunch Settings ...",
+        subtitle: "Show Bunch Preference Pane",
+        arg: `x-bunch://prefs`
+    },
+    {
+        title: "Refresh Your Bunches",
+        subtitle: "Use this if you changed any of your bunch files",
+        arg: `x-bunch://setPref?configDir=${bunchFolder}`
+    }
+];
+
+// Check the config
+if (!bunchFolder) {
+    alfy.error("Missing configuration.");
+}
+else {
+    //Load the bunches and let the user pick
+    glob(bunchFolder + "/**/*.bunch", function (er, files) {
+        var bunches = []
+
+        files.forEach(function (value, index) {
+            if (value.length > bunchFolder.length + 6) {
+                var bunchName = value.substr(bunchFolder.length + 1, value.length - bunchFolder.length - 7)
+                bunches.push(bunchName);
+            }
+        });
+
+        if (alfy.input) {
+            var resultList = alfy
+                .inputMatches(bunches)
+                .map(x => ({
+                    title: x.toLowerCase(),
+                    subtitle: 'Action to run this bunch.',
+                    arg: `x-bunch://${bunchCommand}?bunch=` + x.toLowerCase()
+                }));
+            alfy.output(resultList);
+        } else {
+            var resultList = bunches.map(x => ({
+                title: x.toLowerCase(),
+                subtitle: 'Action to run this bunch.',
+                arg: `x-bunch://${bunchCommand}?bunch=` + x.toLowerCase()
+            }));
+
+            // prepend the bunch settings command and return the list
+            alfy.output(bunchSettings.concat(resultList));
+        }
+    });
+}
